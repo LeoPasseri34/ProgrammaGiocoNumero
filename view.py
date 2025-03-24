9b61d75fdafbc129c480dcdf87183cd2500c4e2f
lpass <s310842@studenti.polito.it>
2025-03-24 18:34:46 +0100

    gioco realizzato

--- view.py
+++ view.py
@@ -12,6 +12,24 @@ class View(object):
         self._titolo = ft.Text("Indovina il numero",
                                color="blue", size=24)
 
+        self._txtOutNMax = ft.TextField(label="N Max", disabled=True, width=200, value=self._controller.getNMax())
+        self._txtOutTMax = ft.TextField(label="T Max", disabled=True, width=200, value=self._controller.getTMax())
+        self._txtOutT = ft.TextField(label="T rimanenti", disabled=True, width=200)
+        self._txtIn = ft.TextField(label="Tentativo", width=200)
+        self._btnReset = ft.ElevatedButton(text ="Nuova Partita", width=200, on_click=self._controller.reset)
+        self._btnPlay = ft.ElevatedButton(text = "Gioca", width=200, on_click=self._controller.play)
+
+        self._lv = ft.ListView(expand=True)
+
+        row1 = ft.Container(self._titolo, alignment=ft.alignment.center)
+
+        row2 = ft.Row([self._txtOutNMax, self._txtOutTMax, self._txtOutT],
+                      alignment=ft.MainAxisAlignment.CENTER)
+
+        row3 = ft.Row([self._btnReset,self._txtIn, self._btnPlay],
+                      alignment=ft.MainAxisAlignment.CENTER)
+
+        self._page.add(row1, row2, row3, self._lv)
 
         self._page.update()
 
